f6d2fe926aac8b20c6b02301300084fdb71a4594
Tomohiro Kawatani <b1017111@fun.ac.jp>
2022-03-21 01:46:15 +0900

    feat:styles camera-ready container

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -1,14 +1,26 @@
-export const cornerButton = { width: '96px', fontSize: '16px' };
+import { autocompleteClasses } from '@mui/material';
+
+const marginContent = 16;
 
 export const selectForm = {
   m: 1,
   width: 500,
   margin: '0 auto',
+  marginTop: marginContent,
 };
 
 export const player = {
-  width: 384,
-  height: 216,
+  width: 480,
+  height: 270,
   border: '1px solid #707070',
   margin: '0 auto',
+  marginTop: marginContent,
+};
+
+export const cornerButton = {
+  width: '96px',
+  fontSize: '16px',
+  marginTop: marginContent,
+  marginLeft: 'auto',
+  marginRight: marginContent,
 };
