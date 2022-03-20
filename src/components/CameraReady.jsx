718d53a51c7cf035d777fecb174c5ff5fc71f0f0
Tomohiro Kawatani <b1017111@fun.ac.jp>
2022-03-21 04:35:43 +0900

    feat: initialize camera setting, select camera

--- src/components/CameraReady.jsx
+++ src/components/CameraReady.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import {
   FormControl,
   InputLabel,
@@ -5,31 +6,61 @@ import {
   MenuItem,
   Button,
 } from '@mui/material';
+import { useEffect, useState } from 'react';
 import { cornerButton, player, selectForm } from '../styles/styles';
+import { getDevices } from '../utils/webCamera';
 
-const MenuProps = {
-  PaperProps: {
-    style: {
-      maxHeight: 48 * 4.5 + 8,
-      width: 250,
-    },
-  },
+const initializeCamera = async (deviceId = null) => {
+  try {
+    const constraints = {
+      audio: false,
+      video: {
+        facingMode: 'environment',
+        width: 1280,
+        height: 720,
+        deviceId: deviceId,
+      },
+    };
+
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    const video = document.getElementById('player');
+    video.srcObject = stream;
+    video.play();
+    return stream;
+  } catch (err) {
+    console.error(`camera error occured: ${err}`);
+  }
 };
 
 const CameraReady = () => {
-  const handleChange = () => {};
+  const [devices, setDevices] = useState(null);
+  const [currentDevice, setCurrentDevice] = useState(null);
+
+  useEffect(async () => {
+    const stream = await initializeCamera();
+    setDevices(await getDevices());
+    setCurrentDevice(stream.id);
+  }, []);
+  const handleChange = (e) => {
+    setCurrentDevice(e.target.value);
+    initializeCamera(e.target.value);
+  };
+
   return (
     <div id="camera-ready" className="main-container">
       <h2 className="main-text">カメラに顔が映るようにしてくだい</h2>
       <FormControl style={selectForm}>
         <InputLabel>ウェブカメラを切り替える</InputLabel>
-        <Select
-          id="demo-simple-select"
-          onChange={handleChange}
-          MenuProps={MenuProps}
-        >
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
+        <Select onChange={handleChange} value={currentDevice} displayEmpty>
+          {devices
+            ? devices.map((device) => {
+                return (
+                  <MenuItem key={device.deviceId} value={device.deviceId}>
+                    {device.label}
+                  </MenuItem>
+                );
+              })
+            : null}
         </Select>
       </FormControl>
       <video id="player" style={player}></video>
---
+++ src/utils/webCamera.js
@@ -0,0 +1,57 @@
+let stream = null;
+let mediaRecorder = null;
+let recordedChunks = [];
+
+export const getDevices = async () => {
+  try {
+    const d = await navigator.mediaDevices.enumerateDevices();
+    const devices = d.filter((device) => device.kind === 'videoinput');
+    return devices;
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+};
+
+export const startRecording = () => {
+  if (!mediaRecorder) {
+    mediaRecorder = new MediaRecorder(stream);
+    mediaRecorder.ondataavailable = (e) => {
+      recordedChunks.push(e.data);
+    };
+  }
+
+  mediaRecorder.start();
+  console.log('MediaRecorder start');
+};
+
+export const stopRecording = () => {
+  mediaRecorder.stop();
+
+  console.log('MediaRecorder stop');
+};
+
+export const pauseRecording = () => {
+  mediaRecorder.pause();
+
+  console.log('MediaRecorder pause');
+};
+
+export const resumeRecording = () => {
+  mediaRecorder.resume();
+
+  console.log('MediaRecorder resume');
+};
+
+export const downloadRecordData = () => {
+  const blob = new Blob(recordedChunks, { type: 'video/webm' });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement('a');
+  document.body.appendChild(a);
+  a.style = 'display: none';
+  a.href = url;
+  a.download = 'video.webm';
+  a.click();
+  window.URL.revokeObjectURL(url);
+  document.body.removeChild(a);
+};
