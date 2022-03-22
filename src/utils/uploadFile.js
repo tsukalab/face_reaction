b82cd0d4357673eba95921a3c3db559ea900b04c
Tomohiro Kawatani <b1017111@fun.ac.jp>
2022-03-22 16:16:12 +0900

    feat:upload file to dropBox

--- src/utils/uploadFile.js
+++ src/utils/uploadFile.js
@@ -1,1 +1,23 @@
 import axios from 'axios';
+
+export const uploadToDropBox = async (file, path) => {
+  try {
+    const res = await axios({
+      method: 'post',
+      url: 'https://content.dropboxapi.com/2/files/upload',
+      data: file,
+      headers: {
+        Authorization:
+          'Bearer sl.BEQagbmPV4VGwOz5LijYEN6MR6a6Wiy5WWgrnya52GSLYFGj6Ie_rwRQPKnGjEkBHmb68gUom5A32knS9993kCjbPLalsCeD17or83R6uKH5XyHWHJHRw2JpSWY5dqr5xa6S6l0',
+        'Content-Type': 'application/octet-stream',
+        'Dropbox-API-Arg': JSON.stringify({
+          path: path, //アップロード時のファイルパス
+          mode: 'overwrite',
+        }),
+      },
+    });
+    console.log(res.statusText);
+  } catch (err) {
+    throw new Error(err);
+  }
+};
